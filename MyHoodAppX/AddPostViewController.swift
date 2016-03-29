c11c14124e9e7dfbbbf92ef171f5a4879eaae5dd
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-03-29 14:32:45 -0500

    Add create post fields error handling

--- MyHoodAppX/AddPostViewController.swift
+++ MyHoodAppX/AddPostViewController.swift
@@ -26,8 +26,30 @@ class AddPostViewController: UIViewController, UIImagePickerControllerDelegate, 
         super.didReceiveMemoryWarning()
     }
     
+    func checkTextField() -> Bool {
+        if titleTextField.text!.isEmpty && descriptionTextField.text!.isEmpty {
+            showErrorMessage("Error", message: "All fields are required")
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func showErrorMessage(title: String, message: String) {
+        let errorMessage = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        errorMessage.addAction(okAction)
+        presentViewController(errorMessage, animated: true, completion: nil)
+    }
+    
     @IBAction func makePostButtonPressed(sender: AnyObject) {
+        if checkTextField() {
+            let title = titleTextField.text!
+            let desc = descriptionTextField.text!
+            var post = Post(imagePath: "", title: title, description: desc)
+        } else {
         
+        }
     }
 
     @IBAction func cancelButtonPressed(sender: AnyObject) {
